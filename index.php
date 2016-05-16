141f1b5493ee3eb9183403b1bcea147494c52172
mobarokhossen <mobarok_hossen@outlook.com>
2016-05-16 12:17:19 +0600

    My profile portfolio....

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Mobarok Hossen</title>
+	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-ui.js"></script>
+	<link rel="shortcut icon" href="img/favicon_2.ico" type="image/x-icon">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="name">
+				<a href="index.php">
+					<span class="my">Mobarok</span>
+					<span class="folio">Hossen</span>
+				</a>
+			</div>
+			<div class="menu">
+				<a href="#" id="menu_bar"><i class="fa fa-bars" aria-hidden="true"></i>Menu</a>
+			</div>
+			<div class="nav">
+				<ul>
+					<li><a href="#home" name="home">Home</a></li>
+					<li><a href="#about">About me</a></li>
+					<li><a href="#service">Service</a></li>
+					<li><a href="#work">My Work</a></li>
+					<li><a href="#contact">Contact Me</a></li>
+				</ul>
+			</div>
+			<script type="text/javascript">
+				$(document).ready(function(){
+						$('#menu_bar').on('click',function() {	
+								$(".nav").fadeToggle(5);
+							});
+					})
+			</script>
+		</div>
+		<div class="banner">
+					<div class="banner_name">
+						<p class="banner_name1">I'm </p>
+						<p class="banner_name2">Mobarok Hossen</p>
+						<p class="banner_name3">Web Developer & Designer,  Front-End-Developer & Back-End-Developer</p>
+						<p class="banner_name4">}</p>
+						<a href="#about">Know more</a>
+					</div>
+		</div>
+		<div class="about">
+			<a name="about"><p class="heading">About Me</p></a>
+			<p class="underline">~~~~~~~~ :: ~~~~~~~~</p>
+			<p>I'm a <span class="span">dynamic web developer</span>  and <span class="span"> creative web designer </span> making awesome website as well as perfect, effective and best performance based identifies website for companies all around the world. 
+			</p>
+			<div class="items">
+				<div class="item">
+					<p><i class="fa fa-laptop"></i></p>
+					<h3>Innovative</h3>
+					<p>Always hungry to create something new and inventive. Searching something different for work.</p>
+				</div>
+				<div class="item">
+					<p><i class="fa fa-briefcase" aria-hidden="true"></i></p>
+					<h3>Hard Worker</h3>
+					<p>My intention is work hard then you become a successful life.Without completing work my mind is not satisfy</p>
+				</div>
+				<div class="item">
+					<p><i class="fa fa-pencil"></i></p>
+					<h3>Quick Learner</h3>
+					<p>Very quick and good learning ability. Good skill to adapting new environment</p>
+				</div>
+				<div class="item">
+					<p><i class="fa fa-clock-o" aria-hidden="true"></i></p>
+					<h3>Time Management</h3>
+					<p>Gathering requirement and prepare task on time with excellence and responsibly</p>
+				</div>
+			</div>
+		</div>
+		<div class="services">
+			<a name="service"><p class="heading">Services</p></a>
+			<p class="underline">~~~~~ :: ~~~~~</p>
+			<p>I will provide you carefully completed your web & desktop application. I will build crative and dynamic website for any companies and industries.</p>
+			<div class="service">
+				<div class="service1">
+					<p><i class="fa fa-forumbee"></i></p>
+					<h3>Software Development</h3>
+					<p>Create offices, companies or industries software. I will bulid JAVA, PHP, MySQL, SQL, C# base software </p>
+				</div>
+				<div class="service1">
+					<p><i class="fa fa-globe" aria-hidden="true"></i></p>
+					<h3>Web development</h3>
+					<p>Develop websites for identifies in globe. Web Framework (codeIgnitor), PHP, MySQL, HTML5, CSS3, Jquery, javascript, Ajax</p>
+				</div>
+				<div class="service1">
+					<p><i class="fa fa-cogs" aria-hidden="true"></i></p>
+					<h3>Web Design</h3>
+					<p>Design using bootstrap, HTML, HTML5, CSS3, Javascript, Jquery, Ajax for looking awesome.</p>
+					</div>
+				<div class="service1">
+					<p><i class="fa fa-wordpress" aria-hidden="true"></i></p>
+					<h3>Wordpress</h3>
+					<p>Wordpress install, theme design, theme customization, wordpress plugins use.</p>
+				</div>
+			</div>
+		</div>
+		<div class="my_works">
+			<a name="work"><p class="heading">My Work</p></a>
+			<p class="underline">~~~~~ :: ~~~~~</p>
+			<p></p>
+			<div class="my_work">
+				<div class="imgs pro1" >
+					<div class="imgstory">
+						<h4>MIDA</h4>
+						<p>Movie information, Download and watching system.</p>
+					</div>
+				</div>
+				<div class="imgs pro2">
+					<div class="imgstory">
+						<h4>Tourism Bangladesh</h4>
+						<p>Tourism Information and Travel Guide</p>
+					</div>
+				</div>
+				<div class="imgs pro3">
+					<div class="imgstory">
+						<h4>Fedrik</h4>
+						<p>Fedrik is online customer support center.</p>
+					</div>
+				</div>
+				<div class="imgs pro4">
+					<div class="imgstory">
+						<h4>Source Business Corporation</h4>
+						<p>Source is a texitle factory. They export there product in european country.</p>
+					</div>
+				</div>
+				<div class="imgs pro5">
+					<div class="imgstory">
+						
+						<h4>Hosting Website</h4>
+						<p>Hosting and Domain purchase website</p>
+					</div>
+				</div>
+				<div class="imgs pro6">
+					<div class="imgstory">
+						<h4>Spectro Media</h4>
+						<p>Spectro Media is a cummunity. Hotel, Restaurant, Fast food and Drinking house.</p>
+					</div>
+				</div>
+				<div class="imgs pro7">
+					<div class="imgstory">
+						<h4>Profile Website</h4>
+						<p>Build Personal and profile website. </p>
+					</div>
+				</div>
+				<div class="imgs pro8">
+					<div class="imgstory">
+						<h4>Motion</h4>
+						<p>Motion is a online news portal.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contact">
+			<a name="contact"><p class="heading">Contact Me</p></a>
+			<p class="underline">~~~~~ :: ~~~~~</p>
+			<p>If you would like to get in touch to discuss about my career profile and to arrange a job interview, you can contact me. You can also hear me as a web developer. So, Please contact me. I will replay you as soon as possible.</p>
+			
+			
+			<div class="message">
+			<?php
+					//if "email" variable is filled out, send email
+					  if (isset($_REQUEST['email']))  {
+					  
+							  //Email information
+							  
+							$admin_email  = "mobarokaiub@gmail.com";
+							$name = $_REQUEST['name'];
+							$message = $_REQUEST['comment'];;
+							$email = $_REQUEST['email'];
+							$subject = $_REQUEST['subject'];
+							  //send email
+					  if (mail($admin_email, "$subject", $name, "From:" . $email)){
+					  
+							//Email response
+							echo "<h3>Send Successfully. Thank you for contacting us. </h3>";
+					  }else
+					  {
+						  echo "<h3> Sorry please file all field or server sending error.</h3>";
+					  }
+					  
+					  }
+					  
+			?>
+				<form action="index.php" method="POST">
+					<input type="text" name="name" class="m_input" placeholder="NAME"/>
+					<input type="email" name="email" class="m_input" placeholder="EMAIL ADDRESS"/>
+					<input type="text" name="subject" class="m_input" placeholder="SUBJECT"/>
+					<textarea cols="30" rows="4" name="comment" placeholder="Message"></textarea></br>
+					<input type="submit" value="submit" class="submit"/>
+				</form>
+			</div>
+		</div>
+		<div class="footer">
+				<p>&copy;Copyright 2014, Powered By: Mobarok Hossen</p>
+		</div>
+	</div>
+</body>
+</html>
